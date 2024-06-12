8cc95c68bf5f07ca5926f17f2ec8897c7dd8b813
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-06-12 15:25:09 +0200

    milestone 4

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,28 @@
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
-    
+
+    data(){
+      return{
+        projects : []
+      }
+    },
+
+    methods:{
+      fetchProjects(){
+        
+        axios.get('http://127.0.0.1:8000/api/projects').then((res)=>{
+          console.log(res.data);
+        })
+      }
+    },
+
+    created(){
+      this.fetchProjects()
+    }
+  
   }
 </script>
 

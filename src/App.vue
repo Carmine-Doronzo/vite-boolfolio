b541ec80bfd346a30568ef21baf91d9e8d7764af
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-06-12 17:56:15 +0200

    Add property at every card

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,65 @@
 <script>
-  import axios from 'axios';
-  import ProjectCardComponent from './components/ProjectCardComponent.vue'
-  import "bootstrap/dist/css/bootstrap.min.css";
-  import 'bootstrap'
-  export default {
-    components:{
-      ProjectCardComponent,
-    },
-    data(){
-      return{
-        projects : [],
-        currentPage : 1,
-        lastPage : null
-      }
-    },
-
-    methods:{
-      nextPage(page){
-        this.currentPage = page
-        this.fetchProjects()
-      },
-
-      fetchProjects(){
-        
-        axios.get('http://127.0.0.1:8000/api/projects',{
-          params:{
-            page: this.currentPage
-
-
-        }}).then((res)=>{
-          
-          this.projects = res.data.results.data
-          this.lastPage = res.data.results.last_page
-          //console.log(this.projects)
-        })
-      }
-    },
-
-    created(){
-      this.fetchProjects()
-      
+import axios from 'axios';
+import ProjectCardComponent from './components/ProjectCardComponent.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap'
+export default {
+  components: {
+    ProjectCardComponent,
+  },
+  data() {
+    return {
+      projects: [],
+      currentPage: 1,
+      lastPage: null
     }
-  
+  },
+
+  methods: {
+    nextPage(page) {
+      this.currentPage = page
+      this.fetchProjects()
+    },
+
+    fetchProjects() {
+
+      axios.get('http://127.0.0.1:8000/api/projects', {
+        params: {
+          page: this.currentPage
+
+
+        }
+      }).then((res) => {
+
+        this.projects = res.data.results.data
+        this.lastPage = res.data.results.last_page
+        //console.log(this.projects)
+      })
+    }
+  },
+
+  created() {
+    this.fetchProjects()
+
   }
+
+}
 </script>
 
 <template>
-  <ul class="row">
-    <ProjectCardComponent v-for="projectItem in projects" :key="projectItem.id" :item="projectItem"/>
-  </ul>
+  <div class="container py-4">
+    <ul class="row gy-2 gap-2 justify-content-center p-0">
+      <ProjectCardComponent v-for="projectItem in projects" :key="projectItem.id" :item="projectItem" />
+    </ul>
 
-  <div v-if="lastPage > 1">
-    <ul class="list-unstyled">
-      <li @click="nextPage(page)" v-for="page in lastPage" :key="page">
+
+    <ul class="list-unstyled d-flex gap-3 justify-content-center" v-if="lastPage > 1">
+      <li style="cursor: pointer;" class="badge text-bg-success" @click="nextPage(page)" v-for="page in lastPage" :key="page">
         {{ page }}
       </li>
     </ul>
+
   </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/components/ProjectCardComponent.vue
+++ src/components/ProjectCardComponent.vue
@@ -1,23 +1,28 @@
 <script>
-  export default {
-    props: {
-      item: { type: Object}
-      
-    }
+export default {
+  props: {
+    item: { type: Object }
+
   }
+}
 </script>
 
 <template>
-  <div>
-    <li class="card">
-      <p>repo name: {{ item.name }}</p>
 
-    </li>
-  </div>
+  <li class="card col-3 ">
+    <p>repo name: {{ item.name }}</p>
+    <p>description: {{ item.description }}</p>
+    
+    <p v-if="item.type_id === null">type: no category</p>
+    <p v-else>type: {{ item.type.name }}</p>
+
+    <p>technologies:</p>
+    <ul style="padding-left: 120px;">
+      <li v-for="tag in item.technologies">{{ tag.name }}</li>
+    </ul>
+    
+  </li>
+
 </template>
 
-<style lang="scss" scoped>
-
-
-
-</style>
+<style lang="scss" scoped></style>

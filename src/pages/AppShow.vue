26a0e0fd1ad531a1c4b689bcc2521b78836cd2a0
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-06-13 20:36:20 +0200

    bonus 2 print the result on console

--- src/pages/AppShow.vue
+++ src/pages/AppShow.vue
@@ -11,11 +11,25 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     methods: {
         goBack() {
             this.$router.back()
-        }
+        },
+        getProject(){
+            axios.get(`http://127.0.0.1:8000/api/projects/${this.$route.params.slug}`).then((res) => {
+
+                // this.projects = res.data.results.data
+                // this.lastPage = res.data.results.last_page
+                console.log(res.data.results)
+            })
+        },
+        
+    },
+    mounted(){
+        console.log(this.$route.params.slug)
+        this.getProject()
     }
 }
 </script>
